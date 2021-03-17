06e41d215a4cb79cf0bd1ebb9e4709e023fe4bad
Ruslan Rakhimov <rakhimov.ruslan.r@yandex.ru>
2021-03-17 19:56:01 +0600

    update brain_game.py

--- brain_games/scripts/brain_game.py
+++ brain_games/scripts/brain_game.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from brain_games.cli import welcome_user
+
 
 def welcome():
     print('Welcome to the Brain Games!')
@@ -6,6 +8,7 @@ def welcome():
 
 def main():
     welcome()
+    welcome_user()
 
 
 if __name__ == '__main__':
