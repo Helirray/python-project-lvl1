2e500d8eacc6fd75c9e419a526fcc2865d54a0b1
Ruslan Rakhimov <rakhimov.ruslan.r@yandex.ru>
2021-03-23 16:13:12 +0600

    update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,11 @@
-from brain_games.cli1 import welcome_user_and_get_user_name, check_even
+from games.engine import welcome_user_and_get_user_name, congratulate_user
+from games.even_game import check_even
 
 
 def main():
     user_name = welcome_user_and_get_user_name()
     check_even(user_name)
+    congratulate_user(user_name)
 
 
 if __name__ == '__main__':
