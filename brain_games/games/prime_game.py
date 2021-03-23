628181a75e502b33c63d5d61869bf42fb2f0d273
Ruslan Rakhimov <rakhimov.ruslan.r@yandex.ru>
2021-03-23 22:47:14 +0600

    add new game brain_prime.py

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,16 @@
+from brain_games.games.engine import is_prime, check_answer
+from random import randint
+
+
+def check_prime(user_name):
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    count = 0
+    while count < 3:
+        num = randint(2, 200)
+        print(f'Question: {num}')
+        correct_answer = 'yes' if is_prime(num) else 'no'
+        answer = input()
+        if check_answer(answer, correct_answer, user_name):
+            count += 1
+        else:
+            count = 0
