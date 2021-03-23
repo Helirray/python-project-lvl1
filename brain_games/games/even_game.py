67092277fc3434e42eede38d4e8df6bb551eaedf
Ruslan Rakhimov <rakhimov.ruslan.r@yandex.ru>
2021-03-23 20:16:41 +0600

    update even_game.py

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -1,4 +1,4 @@
-from brain_games.games.engine import is_even
+from brain_games.games.engine import is_even, check_answer
 from random import randint
 
 
@@ -9,16 +9,8 @@ def check_even(user_name):
         number = randint(1, 100)
         print(f'Question: {number}')
         answer = input()
-        print(f'Your answer: {answer}')
-        if is_even(number) and answer == 'yes' or \
-                is_even(number) is False and answer == 'no':
-            print('Correct!')
+        correct_answer = 'yes' if is_even(number) else 'no'
+        if check_answer(answer, correct_answer, user_name):
             count += 1
-        elif is_even(number) and answer != 'yes':
-            print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes' \n"
-                  f"Let's try again, {user_name}!")
-            count = 0
-        elif is_even(number) is False and answer != 'no':
-            print(f"'{answer}' is wrong answer ;(. Correct answer was 'no' \n"
-                  f"Let's try again, {user_name}!")
+        else:
             count = 0
