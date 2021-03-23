4623e394a63a98855922189da383cc2c2a6b2f1b
Ruslan Rakhimov <rakhimov.ruslan.r@yandex.ru>
2021-03-23 22:47:30 +0600

    update engine.py

--- brain_games/games/engine.py
+++ brain_games/games/engine.py
@@ -65,5 +65,14 @@ def generate_progression():
     return correct_answer
 
 
+def is_prime(num):
+    i = 2
+    while i < num:
+        if num % i == 0:
+            return False
+        i += 1
+    return True
+
+
 def congratulate_user(user_name):
     print(f'Congratulations, {user_name}!')
