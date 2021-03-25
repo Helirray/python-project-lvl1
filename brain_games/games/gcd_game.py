5cf49f80a1146bd5975e1175d986731c6b95bd92
Ruslan Rakhimov <rakhimov.ruslan.r@yandex.ru>
2021-03-25 14:55:00 +0600

    fix brain gcd game

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -1,13 +1,17 @@
-from brain_games.games.engine import generate_gcd, check_answer
+from random import randint
+from copy import copy
+
+START_GAME_MESSAGE = 'Find the greatest common divisor of given numbers.'
 
 
-def check_gcd(user_name):
-    print('Find the greatest common divisor of given numbers.')
-    count = 0
-    while count < 3:
-        correct_answer = generate_gcd()
-        answer = input()
-        if check_answer(answer, correct_answer, user_name):
-            count += 1
+def generate_game_values():
+    num_1 = randint(1, 100)
+    num_2 = randint(1, 100)
+    num_3 = copy(num_1)
+    num_4 = copy(num_2)
+    while num_1 != 0 and num_2 != 0:
+        if num_1 > num_2:
+            num_1 %= num_2
         else:
-            return
+            num_2 %= num_1
+    return f'{num_3} {num_4}', num_1 + num_2
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,12 +1,9 @@
-from brain_games.games.engine\
-    import welcome_user_and_get_user_name, congratulate_user
-from brain_games.games.gcd_game import check_gcd
+from brain_games.games.engine import engine
+from brain_games.games import gcd_game
 
 
 def main():
-    user_name = welcome_user_and_get_user_name()
-    check_gcd(user_name)
-    congratulate_user(user_name)
+    engine(gcd_game)
 
 
 if __name__ == '__main__':
